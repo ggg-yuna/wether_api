4107215fd81cd0d3e95962f890b546d64680e6a7
ggg-yuna <gushikenyuna@gmail.com>
2022-06-23 21:01:01 +0900

    特定地域の情報を変更

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ fetch(url)
     })
     .then(function(weather) {
         console.log(weather);
-        // 特定の地域(今回は東京)だけ選択して変数に詰め直す
+
+        // 特定の地域(今回は沖縄)だけ選択して変数に詰め直す
         let area = weather[0].timeSeries[0].areas[0];
         console.log(area); 
+
+
         // 発表者と報告日時の情報を画面に書き出す
-        document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
-        document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
+        // document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
+        // document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
+
+
         // 特定地域の情報を画面に書き出す
-        document.getElementById("targetArea").lastElementChild.textContent = area.area.name;
-        document.getElementById("today").lastElementChild.textContent = area.weathers[0];
-        document.getElementById("tomorrow").lastElementChild.textContent = area.weathers[1];
-        document.getElementById("dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
+        document.getElementById("wether-targetArea").lastElementChild.textContent = area.area.name;
+        document.getElementById("wether-today").lastElementChild.textContent = area.weathers[0];
+        document.getElementById("wether-tomorrow").lastElementChild.textContent = area.weathers[1];
+        document.getElementById("wether-dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
     });
 
